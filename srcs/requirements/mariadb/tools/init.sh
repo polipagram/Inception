2349e9febc855452094b61079873cf202be35b4a
polipagram <bouarfakawtar68@gmail.com>
2026-09-04 00:44:04 +0100

    improved init script

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+set -e
+
+: "${MYSQL_DATABASE:?Missing database}"
+: "${MYSQL_USER:?Missing user}"
+
+if [ ! -f "/run/secrets/root_passwd" ]; then
+    exit 1
+fi
+
+if [ ! -f "/run/secrets/user_passwd" ]; then
+    exit 1
+fi
+
+MYSQL_ROOT_PASSWD=$(cat /run/secrets/root_passwd | tr -d '\r\n')
+MYSQL_PASSWD=$(cat /run/secrets/user_passwd | tr -d '\r\n')
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
-exec "$@"d
+exec "$@"
