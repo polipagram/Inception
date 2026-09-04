90312f18bf1c436218d3ff67875bc7634f574904
polipagram <bouarfakawtar68@gmail.com>
2026-09-04 03:12:02 +0100

    maridaDB setup is done

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -18,6 +18,14 @@ MYSQL_PASSWD=$(cat /run/secrets/user_passwd | tr -d '\r\n')
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+    mariadbd --user=mysql --bootstrap <<EOF
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWD}';
+CREATE DATABASE IF NOT EXISTS `${MYSQL_DATABASE}`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWD}';
+GRANT ALL PRIVILEGES ON `${MYSQL_DATABASE}`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
 fi
 
 mkdir -p /run/mysqld
